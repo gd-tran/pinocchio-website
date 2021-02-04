a11c26b2a0834034bbfb5124e2b2ffbc1766859e
Giao Tran <58094472+gd-tran@users.noreply.github.com>
2021-02-04 01:32:31 -0800

    added links for button downloads

--- components/Installation.js
+++ components/Installation.js
@@ -6,9 +6,9 @@ export default function Installation(){
   return(
     <div className={styles.main}>
       <div className={styles.download}>
-        <Link href='https://downloadlink/'><button className={styles.magnifyBorder}>Mac</button></Link>
-        <Link href='https://downloadlink/'><button className={styles.magnifyBorder}>Windows</button></Link>
-        <Link href='https://downloadlink/'><button className={styles.magnifyBorder}>Linux</button></Link>
+        <Link href='https://github.com/oslabs-beta/pinocchio/releases/download/v1.0.0-macos/pinocchio-1.0.0.dmg'><button className={styles.magnifyBorder}>Mac</button></Link>
+        <Link href='https://github.com/oslabs-beta/pinocchio/releases/download/v1.0.0-win/pinocchio.Setup.1.0.0.exe'><button className={styles.magnifyBorder}>Windows</button></Link>
+        <Link href='https://github.com/oslabs-beta/pinocchio/releases/download/v1.0.0-lin/pinocchio_1.0.0_amd64.deb'><button className={styles.magnifyBorder}>Linux</button></Link>
       </div>
       {/* <h2 className={styles.textTwo}>Remember to
       </h2>
